4051b096a1be2debe4572ef59984ae83c5f776fd
Karona24 <noeunkarona354@gmail.com>
2026-09-26 12:51:38 +0700

    remove disable: 'mobile' in main.js and update style.css

--- src/main.js
+++ src/main.js
@@ -4,27 +4,22 @@ import router from './router'
 
 import './assets/style.css'
 
-// Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-// AOS
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-
 
 AOS.init({
   duration: 1000,
   once: true,
-  offset: 100,
-  disable: 'mobile' 
+  offset: 100
 })
 
 const app = createApp(App)
 
-
-app.config.performance = false;
+app.config.performance = false
 app.use(router)
 
 app.mount('#app')
